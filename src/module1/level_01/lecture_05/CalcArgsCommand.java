8c163344cffd0699fe23f29ddb64b6f0f75b2cb4
Anton V. <averagev@mail.ru>
2026-01-12 22:40:30 +0300

    CalcArgsCommand 12-01

--- src/module1/level_01/lecture_05/CalcArgsCommand.java
+++ src/module1/level_01/lecture_05/CalcArgsCommand.java
@@ -1,9 +1,22 @@
 // package module1.level_01.lecture_05;
 
+// КАЛЬКУЛЯТОР - ПЕРЕДАЧА АРГУМЕНТОВ В МЕТОД MAIN ИЗ КОМАНДНОЙ СТРОКИ
 public class CalcArgsCommand {
 
     public static void main(String[] args) {
-        System.out.println("Длинна массива args: " + args.length);
+
+        while (true) {
+            if (args.length != 3) {
+                System.out.println("Передайте 3 аргумента в метод main");
+                return;
+            } else {
+                for (String s: args)
+                    System.out.println(s);
+                break;
+            }
+        }
+
+        // System.out.println("Длинна массива args: " + args.length);
     }
 
 }
